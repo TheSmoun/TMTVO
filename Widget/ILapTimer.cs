ba67c58f8b384f551faca3cf4f34880e9b4cd9bf
TheSmoun <SimonGrossmann@Simons-MacBook-Pro.local>
2014-06-02 18:42:01 +0200

    Added interface for lap timers.

---
+++ Widget/ILapTimer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMTVO.Data;
+
+namespace TMTVO.Widget
+{
+    public interface ILapTimer : IWidget
+    {
+        Stopwatch StopWatch { get; }
+        Driver Driver { get; set; }
+        void Update();
+    }
+}
